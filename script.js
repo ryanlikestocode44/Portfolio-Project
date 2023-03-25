2f15589d0ba9b0691e47ba8cd656ad72971f2678
ryanlikestocode44 <nicholasryan770@gmail.com>
2023-03-25 23:32:27 +0700

    Fixed script.js

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 let display = document.querySelector('.display');
-let buttons = Array.from(document.querySelector('td'));
+let buttons = Array.from(document.querySelectorAll('.btn'));
 
+console.log('test')
 buttons.map(button => {
     button.addEventListener('click', (e) => {
+        console.log(e);
+        console.log(e.target.innerText);
         switch(e.target.innerText){
             case 'AC':
                 display.innerText = '';
                 break;
-            case 'AC':
+            case '=':
                 try {
                     display.innerText = eval(display.innerText);
                 } catch {
                     display.innerText = 'Input Error'
                 }
+                break;
             case '&larr;':
                 if (display.innerText){
                     display.innerText = display.innerText.slice(0, -1);
                 }
                 break;
             default:
-                display.innerText += e.display.innerText;
+                display.innerText += e.target.innerText;
         }
     })
 })
